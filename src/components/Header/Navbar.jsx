4518ae360f67619a952f3369c23af53c0f74cba3
kawsarkabir <devkawsarkabir@gmail.com>
2023-09-27 22:06:36 +0600

    navbar responsive update

--- src/components/Header/Navbar.jsx
+++ src/components/Header/Navbar.jsx
@@ -2,41 +2,33 @@ import { Link, NavLink } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <div>
-      <nav className="border-gray-200 bg-gray-50 dark:bg-gray-800 dark:border-gray-700">
-        <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-          <Link href="#" className="flex items-center">
-            <img
-              src="https://i.ibb.co/kQsgggy/Logo.png"
-              className="h-10 mr-3"
-              alt="Logo"
-            />
-          </Link>
-          <button
-            data-collapse-toggle="navbar-solid-bg"
-            type="button"
-            className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-            aria-controls="navbar-solid-bg"
-            aria-expanded="false"
-          >
-            <span className="sr-only">Open main menu</span>
-            <svg
-              className="w-5 h-5"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 17 14"
+    <nav>
+      <div className="navbar max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <label tabIndex={0} className="btn btn-ghost lg:hidden">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />
+              </svg>
+            </label>
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
             >
-              <path stroke="currentColor" d="M1 1h15M1 7h15M1 13h15" />
-            </svg>
-          </button>
-          <div
-            className="hidden w-full md:block md:w-auto"
-            id="navbar-solid-bg"
-          >
-            <ul className="flex flex-col font-medium mt-4 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-transparent dark:bg-gray-800 md:dark:bg-transparent dark:border-gray-700">
               <li>
                 <NavLink
+                  style={{ fontSize: "19px" }}
                   className={({ isActive, isPending }) =>
                     isActive
                       ? "underline text-red-500"
@@ -52,6 +44,7 @@ const Navbar = () => {
               </li>
               <li>
                 <NavLink
+                  style={{ fontSize: "19px" }}
                   className={({ isActive, isPending }) =>
                     isActive
                       ? "underline text-red-500"
@@ -67,6 +60,7 @@ const Navbar = () => {
               </li>
               <li>
                 <NavLink
+                  style={{ fontSize: "19px" }}
                   className={({ isActive, isPending }) =>
                     isActive
                       ? "underline text-red-500"
@@ -82,9 +76,68 @@ const Navbar = () => {
               </li>
             </ul>
           </div>
+          <Link href="#" className="flex items-center">
+            <img
+              src="https://i.ibb.co/kQsgggy/Logo.png"
+              className="h-10 mr-3"
+              alt="Logo"
+            />
+          </Link>
         </div>
-      </nav>
-    </div>
+        <div className="navbar-center hidden lg:flex">
+          <ul className="menu menu-horizontal px-1">
+            <li>
+              <NavLink
+                style={{ fontSize: "19px" }}
+                className={({ isActive, isPending }) =>
+                  isActive
+                    ? "underline text-red-500"
+                    : isPending
+                    ? "pending"
+                    : ""
+                }
+                to={`/`}
+                href="#"
+              >
+                Home
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                style={{ fontSize: "19px" }}
+                className={({ isActive, isPending }) =>
+                  isActive
+                    ? "underline text-red-500"
+                    : isPending
+                    ? "pending"
+                    : ""
+                }
+                to={`/donation`}
+                href="#"
+              >
+                Donation
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                style={{ fontSize: "19px" }}
+                className={({ isActive, isPending }) =>
+                  isActive
+                    ? "underline text-red-500"
+                    : isPending
+                    ? "pending"
+                    : ""
+                }
+                to={`/statistics`}
+                href="#"
+              >
+                Statistics
+              </NavLink>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
   );
 };
 
